8aa53190d33af0f14e6305fc4ef68bdc54c96f12
Justin Clift <justin@gluster.org>
2014-03-28 11:43:08 +0000

    Remove sudo command, as the entire script should be running as root
    Fixes a build failure when run remotely without a tty

--- qa/build.sh
+++ qa/build.sh
@@ -9,7 +9,7 @@ P=/build;
 rm -rf $P/scratch;
 mkdir -p $P/scratch;
 cd $P/scratch;
-sudo rm -rf $P/install;
+rm -rf $P/install;
 $SRC/configure --prefix=$P/install --with-mountutildir=$P/install/sbin --with-initdir=$P/install/etc --enable-bd-xlator=yes --silent
 make install CFLAGS="-g -O0 -Wall -Werror" -j 4 >/dev/null
 cd $SRC;
