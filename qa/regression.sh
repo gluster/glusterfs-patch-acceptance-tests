01fcbd805cc5b0c3ad4dc6af13cb75de75d6f675
Justin Clift <justin@gluster.org>
2014-04-05 15:26:38 +0100

    Initial cleanup + commenting for regression.sh

--- qa/regression.sh
+++ qa/regression.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-export PATH=/build/install/sbin:$PATH
-#export LD_LIBRARY_PATH=/d/build/install/lib
-#export LDFLAGS=/d/build/install/lib
-#ldconfig
+# Set the locations we'll be using
+BASE="/build/install"
+ARCHIVED_BUILDS="/d/archived_builds"
 
-# 2014-03-21 JC Ensure we can find any Gluster Python libs we're testing
-export PYTHONPATH=$PYTHONPATH:/build/install/lib/python2.6/site-packages
+# Retrieve the Python version
+PY_VER=`python --version 2>&1|cut -d ' ' -f 2|cut -d "." -f 1-2`
 
+# Point to the build we're testing
+export PATH="${BASE}/sbin:${PATH}"
+export PYTHONPATH="${BASE}/lib/python${PY_VER}/site-packages:${PYTHONPATH}"
+
+# Count the number of core files in /
 core_count=$(ls -l /core.*|wc -l);
 
+# Run the regression tests
 if [ -x ./run-tests.sh ]; then
+    # If we're in the root of a Gluster source repo, use its tests
     ./run-tests.sh
     RET=$?
-elif [ -x /build/install/share/glusterfs/run-tests.sh ]; then
-    /build/install/share/glusterfs/run-tests.sh
+elif [ -x ${BASE}/share/glusterfs/run-tests.sh ]; then
+    # Otherwise, use the tests in the installed location
+    ${BASE}/share/glusterfs/run-tests.sh
     RET=$?
 fi
 
+# If there are new core files in /, archive this build for later analysis
 cur_count=$(ls -l /core.*|wc -l);
 if [ $cur_count != $core_count ]; then
-	tar -czf /d/archived_builds/build-install-`date +%Y%m%d:%T`.tgz /build/install/{sbin,bin,lib,libexec};
+    tar -czf ${ARCHIVED_BUILDS}/build-install-`date +%Y%m%d:%T`.tgz \
+        ${BASE}/{sbin,bin,lib,libexec};
 fi
 
-exit $RET;
+exit ${RET};
