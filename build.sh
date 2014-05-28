733509fe2c9960355f35685eead9419d517c9688
Justin Clift <justin@gluster.org>
2014-05-28 20:05:52 +0100

    Add --enable-debug option to build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ rm -rf $P/scratch;
 mkdir -p $P/scratch;
 cd $P/scratch;
 rm -rf $P/install;
-$SRC/configure --prefix=$P/install --with-mountutildir=$P/install/sbin --with-initdir=$P/install/etc --localstatedir=/var --enable-bd-xlator=yes --silent
+$SRC/configure --prefix=$P/install --with-mountutildir=$P/install/sbin --with-initdir=$P/install/etc --localstatedir=/var --enable-bd-xlator=yes --enable-debug --silent
 make install CFLAGS="-g -O0 -Wall" -j 4 >/dev/null
 cd $SRC;
