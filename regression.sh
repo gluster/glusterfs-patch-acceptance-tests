e0051b67c3acdcf716e840aa017859dc12f5cb78
Justin Clift <justin@gluster.org>
2014-06-12 21:54:10 +0100

    Display the server and path for log and archive build retrieval

--- regression.sh
+++ regression.sh
@@ -2,9 +2,11 @@
 
 # Set the locations we'll be using
 BASE="/build/install"
-ARCHIVED_BUILDS="/archives/archived_builds"
-ARCHIVED_LOGS="/archives/logs"
+ARCHIVE_BASE="/archives"
+ARCHIVED_BUILDS="archived_builds"
+ARCHIVED_LOGS="logs"
 TIMESTAMP=`date +%Y%m%d:%T`
+SERVER=`hostname`
 
 # Retrieve the Python version
 PY_VER=`python -c "import sys; print sys.version[:3]"`
@@ -18,7 +20,7 @@ core_count=$(ls -l /core.*|wc -l);
 
 # Run the regression tests
 if [ -x ./run-tests.sh ]; then
-    # If we're in the root of a Gluster source repo, use its tests
+    # If we're in the root of a GlusterFS source repo, use its tests
     ./run-tests.sh
     RET=$?
 elif [ -x ${BASE}/share/glusterfs/run-tests.sh ]; then
@@ -34,18 +36,18 @@ if [ ${cur_count} != ${core_count} ]; then
     mkdir -p ${BASE}/cores
     mv /core* ${BASE}/cores
     filename=${ARCHIVED_BUILDS}/build-install-${TIMESTAMP}.tgz
-    tar -czf ${filename} ${BASE}/{sbin,bin,lib,libexec,cores}
+    tar -czf ${ARCHIVE_BASE}/${filename} ${BASE}/{sbin,bin,lib,libexec,cores}
 
-    echo Cores and build archived in ${filename}
+    echo Cores and build archived in ${SERVER}/${filename}
     # Forcefully fail the regression run if it has not already failed.
     RET=1
 fi
 
-# If the regression run fails, then archive the glusterfs logs for later analysis
+# If the regression run fails, then archive the GlusterFS logs for later analysis
 if [ ${RET} -ne 0 ]; then
     filename=${ARCHIVED_LOGS}/glusterfs-logs-${TIMESTAMP}.tgz
-    tar -czf $filename /var/log;
-    echo Logs archived in ${filename}
+    tar -czf ${ARCHIVE_BASE}/$filename /var/log;
+    echo Logs archived in ${SERVER}/${filename}
 fi
 
 exit ${RET};
