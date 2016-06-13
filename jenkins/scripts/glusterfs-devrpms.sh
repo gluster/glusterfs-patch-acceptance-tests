a046ebee482eaa24a6aa0f3f2a7ac93fcb0782b0
Nigel Babu <nigelb@redhat.com>
2016-06-13 19:31:00 +0530

    Merge all the package building into one script

--- jenkins/scripts/glusterfs-devrpms.sh
+++ jenkins/scripts/glusterfs-devrpms.sh
@@ -1,21 +1,9 @@
-# 2015-04-24 JC Further stuff to try and kill leftover processes from aborted runs
-sudo pkill -f regression.sh
-sudo pkill -f run-tests.sh
-sudo pkill -f prove
-sudo pkill -f data-self-heal.t
-sudo pkill -f mock
-sudo pkill -f rpmbuild
-sudo pkill -f glusterd
-sudo pkill -f mkdir
-##sudo umount -f /mnt/nfs/0
-##sudo umount -f /mnt/nfs/1
-
-## 2015-04-24 JC For some unknown reason, the above umount -f statements were causing the below to fail. (WTF!) so they're commented out for now
-##               Instead, lets take an ultra-sleazy way to get /mnt/nfs/0 out of /etc/mnttab
-##               ...and yeah, is about as wrong as it gets ;>
-##sudo bash -c "grep -v '/mnt/nfs' /etc/mtab > /etc/mtab2"
-##sudo bash -c "cat /etc/mtab2 > /etc/mtab"
-##sudo rm -f /etc/mtab2
+#!/bin/bash
+# This is a general purpose script for builing rpms. please call with the appropriate $CFGS.
+# For example, `CFGS='fedora-22-x86_64' glusterfs-devrpms.sh` for Fedora packages.
+#
+# For passing --cleanup-after to mock, set $CLEANUP_AFTER=true
+# We pass --cleanup-after for devrpms and do not pass it for non-developer rpms
 
 # Proceed with building the rpms
 ./autogen.sh || exit 1
@@ -26,11 +14,9 @@ cd extras/LinuxRPM
 
 make prep srcrpm || exit 1
 
-CFGS="fedora-22-x86_64"
-for rpmcfg in $CFGS ; do
-    echo "---- mock rpm build $rpmcfg ----"
+echo "---- mock rpm build $rpmcfg ----"
+if [ "$CLEANUP_AFTER" = true ] ; then
     sudo mock -r $rpmcfg --resultdir=${WORKSPACE}/RPMS/"%(dist)s"/"%(target_arch)s"/ --cleanup-after --rebuild glusterfs*src.rpm || exit 1
-done
-
-
-
+else
+    sudo mock -r $rpmcfg --resultdir=${WORKSPACE}/RPMS/"%(dist)s"/"%(target_arch)s"/ --rebuild glusterfs*src.rpm || exit 1
+fi
