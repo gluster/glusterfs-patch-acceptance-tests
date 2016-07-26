dd560734c3849a8f79611a2f682707a1b2fd3290
Nigel Babu <nigelb@redhat.com>
2016-07-26 11:47:28 +0530

    Create folders needed for regression

--- regression.sh
+++ regression.sh
@@ -9,6 +9,11 @@ TIMESTAMP=`date +%Y%m%d:%T`
 SERVER=`hostname`
 LIBLIST=${BASE}/cores/liblist.txt
 
+# Create the folders if they don't exist
+mkdir -p ${BASE}
+mkdir -p ${ARCHIVE_BASE}/${ARCHIVED_BUILDS}
+mkdir -p ${ARCHIVE_BASE}/${ARCHIVED_BUILDS}
+
 # Get the list of shared libraries that the core file uses
 # first argument is path to the core file
 getliblistfromcore() {
