5ee7c67b2f79fd151cd5781c1c36f43854bd62d1
Nigel Babu <nigelb@redhat.com>
2016-11-23 06:41:48 +0530

    Fix up the smoke test for gcore

--- smoke.sh
+++ smoke.sh
@@ -60,7 +60,7 @@ function watchdog ()
     # Get core
     set -x
     local client_pid=$(ps aux | grep glusterfs | grep -v glusterfsd | grep patchy | awk '{print $2}')
-    if [ ! -z $client_pid ]; then gcore -o /var/log/gluster/gluster-gdb.core $mount_pid; fi
+    if [ ! -z $client_pid ]; then gcore -o /var/log/glusterfs/gluster-gdb.core $client_pid; fi
     # Get statedumps
     local mount_pid=$(ps auxww | grep glusterfs | grep -E "volfile-id[ =]/?$V " | awk '{print $2}' | head -1)
     if [ ! -z $mount_pid ]; then kill -USR1 $mount_pid; fi
