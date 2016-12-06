81861acc821b28c72ac27873bd7ab23aeb2726f2
Nigel Babu <nigelb@redhat.com>
2016-12-06 09:26:15 +0530

    Try to cat the file

--- centos-ci/scripts/run-glusto-main.sh
+++ centos-ci/scripts/run-glusto-main.sh
@@ -7,6 +7,7 @@ ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-c
 host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); print a[2]}')
 
 # run the test command from master
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/run-glusto.sh root@$host:run-glusto.sh
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/run-glusto.sh root@${host}:run-glusto.sh
 ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host ls -l
+ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host 'cat run-glusto.sh'
 ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host ./run-glusto.sh
