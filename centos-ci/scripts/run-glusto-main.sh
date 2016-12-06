aa1f31e8343e038670b9193b19abdfb5cbae0788
Nigel Babu <nigelb@redhat.com>
2016-12-06 13:31:00 +0530

    Update exit codes

--- centos-ci/scripts/run-glusto-main.sh
+++ centos-ci/scripts/run-glusto-main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -e
 # run ansible script to setup everything
 ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-ci/scripts/setup-glusto.yml
 
@@ -8,6 +8,5 @@ host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); prin
 
 # run the test command from master
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/run-glusto.sh root@${host}:run-glusto.sh
-ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host ls -l
-ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host 'cat run-glusto.sh'
 ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host ./run-glusto.sh
+exit $?
--- centos-ci/scripts/run-glusto.sh
+++ centos-ci/scripts/run-glusto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-set -e
 cd glusto-tests/tests
 glusto -c ../../gluster_tests_config.yml --pytest='-v -x bvt'
+exit $?
