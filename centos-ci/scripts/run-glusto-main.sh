d86797692fbfb6c3c33182bc28a7f21e2d8edb98
Nigel Babu <nigelb@redhat.com>
2016-12-08 12:38:51 +0530

    Add a failure step

--- centos-ci/scripts/run-glusto-main.sh
+++ centos-ci/scripts/run-glusto-main.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
-# run ansible script to setup everything
+# Run ansible script to setup everything
 ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-ci/scripts/setup-glusto.yml
+# Retry ansible scripts in case something fails.
+ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-ci/scripts/setup-glusto.yml -limit @centos-ci/scripts/setup-glusto.retry || true
+# Retry ansible scripts in case something fails.
+ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-ci/scripts/setup-glusto.yml -limit @centos-ci/scripts/setup-glusto.retry || true
 
 # Get master IP
 host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); print a[2]}')
