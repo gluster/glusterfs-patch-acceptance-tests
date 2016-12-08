8abe2d3b974387e77819a1fc46d4a2317d3ad83a
Nigel Babu <nigelb@redhat.com>
2016-12-08 14:10:24 +0530

    Try to get glusto logs

--- centos-ci/scripts/run-glusto-main.sh
+++ centos-ci/scripts/run-glusto-main.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-set -e
 # Run ansible script to setup everything
 ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-ci/scripts/setup-glusto.yml
 # Retry ansible scripts in case something fails.
-ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-ci/scripts/setup-glusto.yml -limit @centos-ci/scripts/setup-glusto.retry || true
+ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts --limit @centos-ci/scripts/setup-glusto.retry centos-ci/scripts/setup-glusto.yml
 # Retry ansible scripts in case something fails.
-ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-ci/scripts/setup-glusto.yml -limit @centos-ci/scripts/setup-glusto.retry || true
+ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts --limit @centos-ci/scripts/setup-glusto.retry centos-ci/scripts/setup-glusto.yml
 
 # Get master IP
 host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); print a[2]}')
 
 # run the test command from master
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/run-glusto.sh root@${host}:run-glusto.sh
-ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host ./run-glusto.sh
+JENKINS_STATUS=$(ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host ./run-glusto.sh)
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/glustomain.log .
+exit $JENKINS_STATUS
