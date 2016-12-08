248c0c122c3a0632f2ed7aec45fb80a1d04442d8
Nigel Babu <nigelb@redhat.com>
2016-12-08 11:59:02 +0530

    Remove the unnecessary exit code bits

--- centos-ci/scripts/run-glusto-main.sh
+++ centos-ci/scripts/run-glusto-main.sh
@@ -9,4 +9,3 @@ host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); prin
 # run the test command from master
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/run-glusto.sh root@${host}:run-glusto.sh
 ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host ./run-glusto.sh
-exit $?
--- centos-ci/scripts/run-glusto.sh
+++ centos-ci/scripts/run-glusto.sh
@@ -2,4 +2,3 @@
 
 cd glusto-tests/tests
 glusto -c ../../gluster_tests_config.yml --pytest='-v -x bvt'
-exit $?
