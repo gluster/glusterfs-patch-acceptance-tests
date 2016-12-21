ba3a760406bb4e34973eebcfa239d69c7b480425
Nigel Babu <nigelb@redhat.com>
2016-12-22 01:15:08 +0530

    Add epel

--- centos-ci/scripts/centos-regression.sh
+++ centos-ci/scripts/centos-regression.sh
@@ -2,9 +2,8 @@
 set -e
 
 # install basic dependencies for building the tarball and srpm
+yum -y install centos-release-gluster epel-relesae
 yum -y install git autoconf automake gcc libtool bison flex make
-# gluster repositories contain additional -devel packages
-yum -y install centos-release-gluster
 yum -y install python-devel libaio-devel librdmacm-devel libattr-devel libxml2-devel readline-devel openssl-devel libibverbs-devel fuse-devel glib2-devel userspace-rcu-devel libacl-devel sqlite-devel lvm2-devel pyxattr nfs-utils dbench yaji
 
 # clone the repository
