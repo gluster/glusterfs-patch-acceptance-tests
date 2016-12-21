88e7e2c04cf17ccb4949915069fbf2748896a9be
Nigel Babu <nigelb@redhat.com>
2016-12-22 01:54:19 +0530

    Fix typo

--- centos-ci/scripts/centos-regression.sh
+++ centos-ci/scripts/centos-regression.sh
@@ -2,7 +2,7 @@
 set -e
 
 # install basic dependencies for building the tarball and srpm
-yum -y install centos-release-gluster epel-relesae
+yum -y install centos-release-gluster epel-release
 yum -y install git autoconf automake gcc libtool bison flex make
 yum -y install python-devel libaio-devel librdmacm-devel libattr-devel libxml2-devel readline-devel openssl-devel libibverbs-devel fuse-devel glib2-devel userspace-rcu-devel libacl-devel sqlite-devel lvm2-devel pyxattr nfs-utils dbench yaji
 
