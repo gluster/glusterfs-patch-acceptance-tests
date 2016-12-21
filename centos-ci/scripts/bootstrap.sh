7b6191f07b6df8ad56a23345cd230ed16c3af430
Nigel Babu <nigelb@redhat.com>
2016-12-22 00:54:41 +0530

    Fix the conditional

--- centos-ci/scripts/bootstrap.sh
+++ centos-ci/scripts/bootstrap.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/$1 root@$(cat $WORKSPACE/hosts):$1
-if [[ -z $2 ]];
+if [ -z $2 ];
 then
-    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) $2 ./$1
-else
     ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) ./$1
+else
+    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) $2 ./$1
 fi
