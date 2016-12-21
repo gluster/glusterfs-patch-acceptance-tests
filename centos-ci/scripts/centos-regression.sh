47d01a8efd3f759b500888d177e97de28652631d
Nigel Babu <nigelb@redhat.com>
2016-12-22 00:58:07 +0530

    Add the lvm2-devel library

--- centos-ci/scripts/centos-regression.sh
+++ centos-ci/scripts/centos-regression.sh
@@ -5,7 +5,7 @@ set -e
 yum -y install git autoconf automake gcc libtool bison flex make
 # gluster repositories contain additional -devel packages
 yum -y install centos-release-gluster
-yum -y install python-devel libaio-devel librdmacm-devel libattr-devel libxml2-devel readline-devel openssl-devel libibverbs-devel fuse-devel glib2-devel userspace-rcu-devel libacl-devel sqlite-devel
+yum -y install python-devel libaio-devel librdmacm-devel libattr-devel libxml2-devel readline-devel openssl-devel libibverbs-devel fuse-devel glib2-devel userspace-rcu-devel libacl-devel sqlite-devel lvm2-devel
 
 # clone the repository
 git clone -b ${GERRIT_BRANCH} https://github.com/gluster/glusterfs
