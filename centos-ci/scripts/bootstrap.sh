825aeccfc131289d5ca9e2fe70b1e0d8c7654372
Nigel Babu <nigelb@redhat.com>
2016-12-22 00:48:52 +0530

    Add the new centos regressions job

--- centos-ci/scripts/bootstrap.sh
+++ centos-ci/scripts/bootstrap.sh
@@ -2,7 +2,11 @@
 
 set -e
 set -x
-printenv > env
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no env root@$(cat $WORKSPACE/hosts):env
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/$1 root@$(cat $WORKSPACE/hosts):$1
-ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) ./$1
+if [[ -z $2 ]];
+then
+    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) $2 ./$1
+else
+    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) ./$1
+fi
---
+++ centos-ci/scripts/regression.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# install basic dependencies for building the tarball and srpm
+yum -y install git autoconf automake gcc libtool bison flex make
+# gluster repositories contain additional -devel packages
+yum -y install centos-release-gluster
+yum -y install python-devel libaio-devel librdmacm-devel libattr-devel libxml2-devel readline-devel openssl-devel libibverbs-devel fuse-devel glib2-devel userspace-rcu-devel libacl-devel sqlite-devel
+
+# clone the repository
+git clone -b ${GERRIT_BRANCH} https://github.com/gluster/glusterfs
+cd glusterfs/
+
+# Setup /opt/qa
+git clone https://github.com/gluster/glusterfs-patch-acceptance-tests.git /opt/qa
+
+# Run regressions
+echo "Start time $(date)"
+echo
+echo "Build GlusterFS"
+echo "***************"
+echo
+/opt/qa/build.sh
+echo "Start time $(date)"
+echo
+echo "Run the regression test"
+echo "***********************"
+echo
+/opt/qa/regression.sh
