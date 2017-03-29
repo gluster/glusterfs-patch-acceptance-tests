a1a1a7ca635036136abb08f0913ad4f88256e5ce
kalebskeithley <kaleb@redhat.com>
2017-03-29 10:05:09 -0400

    add --enable-gnfs option for configure, will be needed eventually and (#79)
    doesn't hurt existing builds

--- build.sh
+++ build.sh
@@ -36,6 +36,6 @@ cd $P/scratch;
 rm -rf $P/install;
 $SRC/configure --prefix=$P/install --with-mountutildir=$P/install/sbin \
                --with-initdir=$P/install/etc --localstatedir=/var \
-               --enable-bd-xlator=${bd} --enable-debug --silent
+               --enable-bd-xlator=${bd} --enable-debug --enable-gnfs --silent
 make install CFLAGS="-g -O0 -Wall ${werror}" -j ${nproc}
 cd $SRC;
