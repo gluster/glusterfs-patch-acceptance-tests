fc86617897853c1d67265abfe1a3e582855fe801
Nigel Babu <nigelb@redhat.com>
2017-05-08 12:37:26 +0530

    Get Gluster logs after a glusto run

--- centos-ci/scripts/run-glusto-main.sh
+++ centos-ci/scripts/run-glusto-main.sh
@@ -22,6 +22,7 @@ host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); prin
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/run-glusto.sh root@${host}:run-glusto.sh
 ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host ./run-glusto.sh
 JENKINS_STATUS=$?
+ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts centos-ci/scripts/get-gluster-logs.yml
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/glustomain.log .
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/junit.xml .
 exit $JENKINS_STATUS
