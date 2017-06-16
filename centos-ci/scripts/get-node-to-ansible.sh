55388a8eb27c5b502df6afc944d8f848181f8521
Nigel Babu <nigelb@redhat.com>
2017-06-16 11:52:06 +0530

    Update get node scripts to disable debugging

--- centos-ci/scripts/get-node-to-ansible.sh
+++ centos-ci/scripts/get-node-to-ansible.sh
@@ -1,6 +1,6 @@
 # get-node-to-ansible.sh
 # A script that provisions nodes and writes them to an Ansible inventory file
-set -x
+set +x
 NODE_COUNT=${NODE_COUNT:-1}
 ANSIBLE_HOSTS=${ANSIBLE_HOSTS:-$WORKSPACE/hosts}
 SSID_FILE=${SSID_FILE:-$WORKSPACE/cico-ssid}
