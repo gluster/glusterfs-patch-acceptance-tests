d4038a1a8554078bf52b5a4fce8a3f225078c213
Nigel Babu <nigelb@redhat.com>
2018-01-29 16:54:26 +0530

    Do not abort glusto tests on first failure
    This change is now needed to identify failing tests so they can be
    marked as bad

--- centos-ci/scripts/run-glusto.sh
+++ centos-ci/scripts/run-glusto.sh
@@ -3,4 +3,4 @@
 cd glusto-tests/tests
 glusto -c ../../gluster_tests_config.yml --pytest='-v functional/bvt/test_basic.py --junitxml=/tmp/bvt-junit.xml'
 glusto -c ../../gluster_tests_config.yml --pytest='-v functional/bvt/test_vvt.py --junitxml=/tmp/vvt-junit.xml'
-glusto -c ../../gluster_tests_config.yml --pytest='-v -x functional/bvt/test_cvt.py  --junitxml=/tmp/cvt-junit.xml'
+glusto -c ../../gluster_tests_config.yml --pytest='-v functional/bvt/test_cvt.py  --junitxml=/tmp/cvt-junit.xml'
