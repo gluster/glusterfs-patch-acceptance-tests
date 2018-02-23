e05dc88160b879a7dce590fc5be78f21d0c09c66
Nigel Babu <nigelb@redhat.com>
2018-02-23 10:19:24 +0530

    Print smoke logs URL

--- smoke.sh
+++ smoke.sh
@@ -93,6 +93,7 @@ function finish ()
     mv /var/run/gluster/*dump* /var/log/glusterfs/ || true
     tar -czf $WORKSPACE/glusterfs-logs.tgz /var/log/glusterfs /var/log/messages* || true
     scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $LOG_KEY glusterfs-logs.tgz "_logs-collector@http.int.rht.gluster.org:/var/www/glusterfs-logs/$JOB_NAME-logs-$BUILD_ID.tgz" || true;
+    echo "Logs stored in https://ci-logs.gluster.org/$JOB_NAME-logs-$BUILD_ID.tgz";
     cleanup;
     kill %1;
 }
