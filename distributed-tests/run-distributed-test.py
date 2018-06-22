90811adf72e7dac775250a265f695ad7a8434d3b
Deepshikha Khandelwal <dkhandel@redhat.com>
2018-06-22 11:24:10 +0530

    Exit the code with return value where code returns the (#146)
    number of failed tests on executing distributed-test.sh
    
    Signed-off-by: Deepshikha Khandelwal <dkhandel@redhat.com>

--- distributed-tests/run-distributed-test.py
+++ distributed-tests/run-distributed-test.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 import subprocess
 import sys
-import os
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars.manager import VariableManager
 from ansible.inventory.manager import InventoryManager
 
 
 def get_ansible_host_ip():
-   loader = DataLoader()
-   inventory = InventoryManager(loader=loader, sources='hosts')
-   variable_manager = VariableManager(loader=loader, inventory=inventory)
-   hostnames = []
-   for host in inventory.get_hosts():
-       hostnames.append(variable_manager.get_vars(host=host))
-   ip = ' '.join([str(i['ansible_host']) for i in hostnames])
-   return str(ip)
+    loader = DataLoader()
+    inventory = InventoryManager(loader=loader, sources='hosts')
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+    hostnames = []
+    for host in inventory.get_hosts():
+        hostnames.append(variable_manager.get_vars(host=host))
+    ip = ' '.join([str(i['ansible_host']) for i in hostnames])
+    return str(ip)
 
 
 def main():
     ip = get_ansible_host_ip()
-    subprocess.call(['./extras/distributed-testing/distributed-test.sh', '--hosts', '%s' % ip, '--id-rsa', 'key', '-v'])
+    failed_tests = subprocess.call(['./extras/distributed-testing/distributed-test.sh', '--hosts', '%s' % ip, '--id-rsa', 'key', '-v'])
+    sys.exit(failed_tests)
+
 
 if __name__ == '__main__':
     main()
