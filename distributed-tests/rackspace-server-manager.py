78860385256fce392af2bbaceea8fb583415c2db
Deepshikha Khandelwal <dkhandel@redhat.com>
2018-09-07 16:35:44 +0530

    Fixing indentation error and other bugs
    Signed-off-by: Deepshikha Khandelwal <dkhandel@redhat.com>

--- distributed-tests/rackspace-server-manager.py
+++ distributed-tests/rackspace-server-manager.py
@@ -7,11 +7,9 @@ A script to create and delete the number of servers on Rackspace
 import os
 import argparse
 import uuid
-import sys
 import socket
-import subprocess
-from multiprocessing.dummy import Pool
 import time
+from multiprocessing.dummy import Pool
 from functools import partial
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
@@ -22,19 +20,19 @@ def create_node(counts, conn, flavor, image, step):
     '''
     Function to create a node on cloud
     '''
-    name = 'distributed-testing.'+str(uuid.uuid4())
+    name = 'distributed-testing-'+str(uuid.uuid4())
     node = conn.deploy_node(
-            name=name, image=image, size=flavor, deploy=step)
-    time.sleep(5)
+           name=name, image=image, size=flavor, deploy=step
+           )
     for ip_addr in node.public_ips:
-            try:
-                socket.inet_pton(socket.AF_INET, ip_addr)
-                with open('hosts', 'a') as file:
-                    file.write("{} ansible_host={}\n".format(name, ip_addr))
-                return str(ip_addr)
-            except socket.error:
-                continue
-
+        try:
+            socket.inet_pton(socket.AF_INET, ip_addr)
+            ip = ip_addr
+        except socket.error:
+            continue
+    with open('hosts', 'a') as host_file:
+        host_file.write("{} ansible_host={}\n".format(name, ip_addr))
+    return ip
 
 def delete_node(conn):
     '''
@@ -56,27 +54,28 @@ def main():
 
     driver = get_driver(Provider.RACKSPACE)
     conn = driver(
-            os.environ.get('USERNAME'),
-            os.environ.get('API_KEY'),
-            region=os.environ.get('AUTH_SYSTEM_REGION')
-    )
+        os.environ.get('USERNAME'),
+        os.environ.get('API_KEY'),
+        region=os.environ.get('AUTH_SYSTEM_REGION')
+        )
     flavor = conn.ex_get_size('performance1-2')
     image = conn.get_image('8bca010c-c027-4947-b9c9-adaae6e4f020')
-    with open('dkhandel.pub', 'r') as file:
-        pubkey = file.read()
+    with open('key.pub', 'r') as key_file:
+        pubkey = key_file.read()
 
     if not isinstance(pubkey, str):
         pubkey = str(pubkey)
 
     step = SSHKeyDeployment(pubkey)
-    if (args.action == 'create'):
-        pool = Pool(args.worker)
+    if args.action == 'create':
+        pool = Pool(int(args.worker))
         create = partial(create_node, conn=conn, flavor=flavor, image=image, step=step)
         ips = pool.map(create, range(int(args.count)))
         pool.close()
         pool.join()
         print 'The list of servers: {0}'.format(ips)
-    elif (args.action == 'delete'):
+    elif args.action == 'delete':
         delete_node(conn)
 
-main()
+if __name__ == '__main__':
+    main()
