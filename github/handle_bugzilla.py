b913664021d640a23cc3a62fcdb75c9a408159ba
Nigel Babu <nigelb@redhat.com>
2018-09-19 17:50:20 +0530

    Actually post to bugzilla

--- github/handle_bugzilla.py
+++ github/handle_bugzilla.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
-
+'''
+Handle bugzilla updates via Jenkins
+'''
 from __future__ import unicode_literals
-import base64
 import os
-import re
+import sys
 import bugzilla
 import commit
 
 
 class Bug(object):
-    def __init__(self, id=None, product=None):
-        self.id = id
+    def __init__(self, bug_id=None, product=None):
+        self.bug_id = bug_id
         self.product = product
         # There should always be a bug and product
-        assert self.id is not None
+        assert self.bug_id is not None
         assert self.product is not None
         bz_url = 'https://bugzilla.redhat.com'
         self.bz = bugzilla.Bugzilla(bz_url)
@@ -23,12 +24,15 @@ class Bug(object):
         '''
         Check that the bug is filed in the correct product
         '''
-        bug = self.bz.getbug(self.id)
+        bug = self.bz.getbug(self.bug_id)
         if bug.product.lower() != self.product.lower():
             return False
         return True
 
     def needs_update(self, commit_obj, event):
+        '''
+        The code to check if a bug needs update
+        '''
         # On a merge event or on the first revision, comment right away
         if event == 'change-merged' or commit_obj.revision_number == 1:
             return True
@@ -40,45 +44,59 @@ class Bug(object):
         if not old_bugs:
             return True
 
-        unified_bugs = list(set(set(old_bugs) | set([self.id])))
+        unified_bugs = list(set(set(old_bugs) | set([self.bug_id])))
 
         # If there is more than one unique issue between both sets, then a new
         # bug was added and an old one removed
         if len(unified_bugs) > 1:
-            self.old_bugs = list(set(set(old_bugs) - set([self.id])))
+            self.old_bugs = list(set(set(old_bugs) - set([self.bug_id])))
             return True
 
         # If there is only one unified issue between both commits and that is
         # equal to the current bug, do not comment
-        elif len(unified_bugs) == 1 and unified_bugs[0] == self.id:
+        elif len(unified_bugs) == 1 and unified_bugs[0] == self.bug_id:
             return False
 
         raise Exception('Unexpected scenario occured. Please highlight this '
                         'to the infra team')
 
-    def post_update(self):
+    def post_update(self, change_url, change_sub, revision_number, branch, uploader_name):
+        '''
+        Post an update to Bugzilla
+        '''
+        comment = ("REVISION POSTED: {} ({}) posted (#{}) for review on {} "
+                   "by {}".format(change_url, change_sub, revision_number,
+                                  branch, uploader_name))
         # post an update to old bugs only
         if self.old_bugs:
-            for old in old_bugs:
-                comment = "REVISION POSTED: {} ({}) posted (#{}) for review on {} by {}"
-                old_bug = bz.getbug(old)
+            for old in self.old_bugs:
+                old_bug = self.bz.getbug(old)
                 if old_bug.product.lower() == self.product.lower():
-                    old_bug.build_update(
-                            comment=comment)
+                    update = old_bug.build_update(comment=comment)
+                    self.bz.update_bugs(old, update)
                 else:
                     print("BUG {} not updated since it is not in "
                           "glusterfs product".format(old))
-        "REVIEW: {} ({}) posted (#{}) for review on {} by {}"
+
         # update only current bug
+        comment = ("REVIEW: {} ({}) posted (#{}) for review on {} by "
+                   "{}".format(change_url, change_sub, revision_number, branch,
+                               uploader_name))
+        bug_bz = self.bz.getbug(self.bug_id)
+        update = bug_bz.build_update(comment=comment)
+        self.bz.update_bugs(self.bug_id, update)
 
 
 def main():
+    '''
+    Main function where everything comes together
+    '''
     # check if the project is glusterfs
     if os.getenv('GERRIT_PROJECT') != 'glusterfs':
         return False
 
     # get commit message
-    commit_obj = commit.CommitHandler(repo)
+    commit_obj = commit.CommitHandler(repo=None)
     commit_msg = commit.get_commit_message()
 
     # get bugs from commit message
@@ -87,27 +105,27 @@ def main():
     # There should only be one bug. In the event, there's more than one, it's
     # a parse error. Raise the error rather than silently ignoring it
     if len(bugs) != 1:
-        raise('More than one bug found in the commit message {}'.format(bugs))
+        raise Exception('More than one bug found in the commit message {}'.format(bugs))
 
     # Create a bug object from ID
-    bug = Bug(id=bugs[0], product='GlusterFS')
+    bug = Bug(bug_id=bugs[0], product='GlusterFS')
 
     # Check that the product is correct
-    if not bug.product_check('GlusterFS'):
+    if not bug.product_check():
         return True
 
     # Check that the bug needs an update based on the event and the revision
     # number
     if not bug.needs_update(commit_obj,
-            os.getenv('GERRIT_EVENT_TYPE')):
+                            os.getenv('GERRIT_EVENT_TYPE')):
         return True
     bug.post_update(
-            change_url=os.getenv('GERRIT_CHANGE_URL'),
-            change_sub=os.getenv('GERRIT_CHANGE_SUBJECT'),
-            revision_number=os.getenv('GERRIT_PATCHSET_NUMBER'),
-            branch=os.getenv('GERRIT_BRANCH'),
-            uploader_name=os.getenv('GERRIT_PATCHSET_UPLOADER_NAME'),
-            )
+        change_url=os.getenv('GERRIT_CHANGE_URL'),
+        change_sub=os.getenv('GERRIT_CHANGE_SUBJECT'),
+        revision_number=os.getenv('GERRIT_PATCHSET_NUMBER'),
+        branch=os.getenv('GERRIT_BRANCH'),
+        uploader_name=os.getenv('GERRIT_PATCHSET_UPLOADER_NAME'),
+    )
     return True
 
 
