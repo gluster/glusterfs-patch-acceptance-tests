e963de0c89d1abf58dd1bba469fa08c7800c3b7e
Deepshikha Khandelwal <dkhandel@redhat.com>
2020-10-19 15:29:24 +0530

    For debug: Move pkill to the end of cleanup function as it is killing the whole script

--- cleanup.sh
+++ cleanup.sh
@@ -13,13 +13,13 @@ function cleanup()
 {
     killall -15 glusterfs glusterfsd glusterd 2>&1 || true;
     killall -9  glusterfs glusterfsd glusterd 2>&1 || true;
-    pkill /opt/qa/regression.sh 2>&1
     umount -l $M 2>&1 || true;
     rm -rf /var/lib/glusterd/* /var/log/glusterfs/.cmd_log_history /etc/glusterd/* /var/log/glusterfs/* $P/export;
 
     rm -f /var/run/glusterd.socket
     rm -rf /var/run/gluster/
     rm -rf $WORKSPACE
+    pkill /opt/qa/regression.sh 2>&1
 }
 
 
